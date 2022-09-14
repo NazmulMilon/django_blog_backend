ccb8abf63109c8f5dbab6e203591feeb5b8a4e72
nazmulmilon <milonsarker73@gmail.com>
2022-09-14 10:47:21 +0600

    category and post models updated with help text

--- blog/contents/models.py
+++ blog/contents/models.py
@@ -4,16 +4,20 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=100, help_text='type of post category')
 
     def __str__(self):
         return self.category_name
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    author_name = models.ForeignKey(User, on_delete=models.CASCADE)
-    category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
-    description = models.CharField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, help_text='blog post title name')
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE, help_text='blogs writer name')
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE, help_text='foreignkey relationship with '
+                                                                                    'Category')
+    description = models.TextField(help_text='body of a blog post')
+    created_at = models.DateTimeField(auto_now_add=True, help_text='blog written date')
+    updated_at = models.DateTimeField(auto_now_add=True, help_text='if any update needed')
+
+    def __str__(self):
+        return self.title
--- blog/contents/views.py
+++ blog/contents/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Category, Post
+
 
 # Create your views here.
+
+def homepage(request):
+    post = Post.objects.all()
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'display.html', context)
