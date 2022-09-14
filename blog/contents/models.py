6a2026259dc9bdf37a0dbad79ba2f92520755558
nazmulmilon <milonsarker73@gmail.com>
2022-09-14 10:16:05 +0600

    post model created

--- blog/contents/models.py
+++ blog/contents/models.py
@@ -9,3 +9,11 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name
 
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
