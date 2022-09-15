a8abcd1076969f7b57cee775a47e13e789f59b6b
nazmulmilon <milonsarker73@gmail.com>
2022-09-15 11:02:06 +0600

    post details view.py file write for single api view

--- blog/contents/urls.py
+++ blog/contents/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('contents/', views.post_list),
+    path('contents/<int:pk>', views.post_detail)
 ]
--- blog/contents/views.py
+++ blog/contents/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from .serializers import PostSerializer
 
 
+
 def post_list(request):
     """
     list all posts or create posts
@@ -13,9 +14,32 @@ def post_list(request):
         return JsonResponse(serialize.data, safe=False)
 
     elif request.method == 'POST':
+
         serialize = PostSerializer(data=data)
         if serialize.is_valid():
             serialize.save()
             return JsonResponse(serialize.data, status=201)
         return JsonResponse(serialize.errors, status=400)
 
+
+def post_detail(request, pk):
+    """ retrieve, update or delete code for posts"""
+
+    try:
+        obj = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serialize = PostSerializer(obj)
+        return JsonResponse(serialize.data)
+    elif request.method == 'PUT':
+        serialize = PostSerializer(obj, data=data)
+        if serialize.is_valid():
+            serialize.save()
+            return JsonResponse(serialize.data)
+        return JsonResponse(serialize.errors, status=400)
+
+    elif request.method == 'DELETE':
+        obj.delete()
+        return HttpResponse(status=204)
