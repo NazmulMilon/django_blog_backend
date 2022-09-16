12526d491e0f696fe82f23260e244dbb80120335
nazmulmilon <milonsarker73@gmail.com>
2022-09-16 12:32:21 +0600

    write unit test for Category models

--- blog/contents/tests.py
+++ blog/contents/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Category
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+
+class CategoryTest(TestCase):
+    def create_category(self, title="only a test", body="only a body test"):
+        return Category.objects.create(title=title, body=body, created_at=timezone.now())
+
+    def test_whatever_creation(self):
+        w = self.create_category()
+        self.assertTrue(isinstance(w, Category))
+        self.assertEqual(w, __unicode__(), w.title)
