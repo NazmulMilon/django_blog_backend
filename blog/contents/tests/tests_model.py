e536be42e25c0903f6f48a9723aeb48252a131eb
nazmulmilon <milonsarker73@gmail.com>
2022-09-19 15:52:31 +0600

    unit testing for model class

--- blog/contents/tests/tests_model.py
+++ blog/contents/tests/tests_model.py
@@ -1,31 +1,42 @@
+# from django.contrib.auth.models import User
 from django.test import TestCase
 from ..models import Category, Post
-from django.contrib.auth.models import User
-from ..models import User
 
 
-class CategoryTest(TestCase):
-    def create_category(self, category_name="Only test category name"):
-        return Category.objects.create(category_name=category_name)
-
-    def test_category_creation(self):
-        category_obj = self.create_category()
-        self.assertTrue(isinstance(category_obj, Category))
-        self.assertEqual(category_obj.category_name, category_obj.category_name)
-
-    # class PostTest(TestCase):
-    def test_post_creation(self):
-        user = User.objects.create_user(username='username', email='mail@gmail.com')
-        # user.save()
-        category = Category.objects.create(category_name='post category')
-        # category.save()
-        obj = Post(author_name=user, title="post title", category_name=category, description="post details")
-        # obj.save()
-        self.assertEqual(str(obj), 'post title')
+class TestCategory(TestCase):
+    def test_create_category(self):
+        category_dict={
+            "category_name": 'category name',
+        }
+        obj = Category(**category_dict)
+        self.assertEquals(str(obj), 'category name')
 
 
-
-
+'''Old method to test django models'''
+#
+#
+# class CategoryTest(TestCase):
+#     def create_category(self, category_name="Only test category name"):
+#         return Category.objects.create(category_name=category_name)
+#
+#     def test_category_creation(self):
+#         category_obj = self.create_category()
+#         self.assertTrue(isinstance(category_obj, Category))
+#         self.assertEqual(category_obj.category_name, category_obj.category_name)
+#
+#     # class PostTest(TestCase):
+#     def test_post_creation(self):
+#         user = User.objects.create_user(username='username', email='mail@gmail.com')
+#         # user.save()
+#         category = Category.objects.create(category_name='post category')
+#         # category.save()
+#         obj = Post(author_name=user, title="post title", category_name=category, description="post details")
+#         # obj.save()
+#         self.assertEqual(str(obj), 'post title')
+#
+#
+#
+#
 
 # class TestModel(TestCase):
 #
