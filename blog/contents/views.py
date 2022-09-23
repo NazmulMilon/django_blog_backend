bb11a21bf893c5839e57020cf1d8f712ee1f3231
nazmulmilon <milonsarker73@gmail.com>
2022-09-23 11:05:54 +0600

    write API_Views of delete method for delete single comment of Comment details class

--- blog/contents/views.py
+++ blog/contents/views.py
@@ -126,6 +126,11 @@ class CommentDetail(APIView):
             return Response(serialize.data, status=status.HTTP_200_OK)
         return Response(serialize.errors, status=status.HTTP_404_NOT_FOUND)
 
+    def delete(self, request, pk):
+        queryset= self.get_obj(pk)
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # from django.http import HttpResponse, JsonResponse
 # from .models import Post, Category
