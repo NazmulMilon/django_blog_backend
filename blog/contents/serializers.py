f969ccc17b2988b38c52cb22b96a87052d15c8c9
nazmulmilon <milonsarker73@gmail.com>
2022-09-26 10:54:08 +0600

    write exclude method instead of fields in serializers.py file

--- blog/contents/serializers.py
+++ blog/contents/serializers.py
@@ -5,22 +5,26 @@ from .models import Post, Category, Comment, Reply
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
+        # fields = '__all__'
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
+        # fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
+        # fields = '__all__'
 
         
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
-        fields = '__all__'
+        exclude = ['created_at', 'updated_at']
+        # fields = '__all__'
